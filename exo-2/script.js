c1ca84be6c8556d888364ed7936bc797f4e6db8b
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-01-11 13:29:19 +0100

    fin exo2

--- exo-2/script.js
+++ exo-2/script.js
@@ -16,7 +16,9 @@ let hour = myDate.getUTCHours();
 let today = myDate.getUTCDay();
 
 // Tester si c'est un jour de semaine ou de weekend
-if (today == 5 && hour >= 17) {
+if (today == 1 && hour < 9) {
+    console.log(weekend);
+} else if (today == 5 && hour >= 17) {
     console.log(weekend);
 } else if (today >= 1 && today <= 5) {
     console.log(weekday);
@@ -28,3 +30,21 @@ if (today == 5 && hour >= 17) {
 
 
 //console.log(today, WeekDay);
+
+/* const myDate = new Date();
+let semaine = "Semaine";
+let weekend = "Weekend";
+let jour;
+let heure;
+let isTesting = false;
+if (isTesting) {
+  jour = prompt("Choisisez le jour de la semaine (1-6 lundi-samedi | 0 dimanche)");
+  heure = prompt("Choisisez l'heure (format 24H)");
+} else {
+  jour = myDate.getDay();
+  heure = myDate.getHours();
+}
+
+jour === "6" || jour === "0"?console.log(weekend):console.log(semaine);
+jour === "5" && heure >= "17"?console.log(weekend):console.log(semaine);
+jour === "1" && heure <= "9"?console.log(weekend):console.log(semaine); */
