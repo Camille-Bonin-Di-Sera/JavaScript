e898a4f589797fa31b00d60975101f012358f385
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-01-11 14:52:04 +0100

    exo3.2

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,7 +3,7 @@
 
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
-let SctMsg = secretMessage.pop('');
-
+//let SctMsg = secretMessage.pop('');
+let newObject = secretMessage.push('to', 'program');
 
 console.log('secretMessage', secretMessage);
