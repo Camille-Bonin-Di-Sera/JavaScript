43d4f69a0892bab06eecc714a5320f6196de56d4
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-01-12 13:43:46 +0100

    exo6 final

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -1,13 +1,13 @@
 // Cours 8 de Codecademy
 
 
-var joeInfo = {
+/* var joeInfo = {
     name: "Joe's house",
     rooms: 5,
     garage: false,
     bathrooms: 1 + 2,
     cars: ['Clio', 'Van'],
-};
+}; */
 // ! Ne faites pas de modification au dessus de cette ligne !
 
 // 1. Afficher le nombre de voitures de Joe
@@ -19,3 +19,54 @@ var joeInfo = {
 // 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
     // joeInfo.garage = 1;
     // console.log(joeInfo.garage)
+    let team = {
+        players: [{
+            firstName: "Pablo",
+            lastName: "Sanchez",
+            age: 11
+        }],
+        games: [{
+            opponent: "Broncos",
+            teamPoints: 42,
+            opponentPoints: 27
+        }],
+        addPlayer: function(firstName, lastName, age){
+            this.players.push({
+                firstName: firstName,
+                lastName: lastName,
+                age: age
+            });
+        },
+        addGame: function (opponent, teamPoints, opponentPoints) {
+            this.games.push({
+                opponent: opponent,
+                teamPoints: parseInt(teamPoints),
+                opponentPoints: parseInt(opponentPoints)
+            });
+        }
+    }
+
+// Appeler les fonctions pour ajouter des joueurs et des matchs
+team.addPlayer("John", "Smith", 25);
+team.addPlayer("Jane", "Doe", 20);
+team.addGame("Broncos", 42, 27);
+team.addGame("Patriots", 21, 35);
+
+// Calculer la somme des points de l'équipe sur tous les matchs joués
+const totalTeamPoints = team.games.reduce((acc, game) => acc + game.teamPoints, 0);
+console.log(`Total team points: ${totalTeamPoints}`);
+
+// Calculer la moyenne des points de l'équipe adverse sur tous les matchs
+const averageOpponentPoints = team.games.reduce((acc, game) => acc + game.opponentPoints, 0) / team.games.length;
+console.log(`Average opponent points: ${averageOpponentPoints}`);
+
+// Trouver le joueur le plus âgé
+const oldestPlayer = team.players.sort((a, b) => b.age - a.age)[0];
+console.log(`Oldest player: ${oldestPlayer.firstName} ${oldestPlayer.lastName}, ${oldestPlayer.age} years old`);
+
+// Bonus : Trier les joueurs par ordre alphabétique
+const sortedPlayers = team.players.sort((a, b) => a.lastName.localeCompare(b.lastName));
+console.log(`Sorted players: ${sortedPlayers.map(p => p.lastName)}`);
+
+
+    
