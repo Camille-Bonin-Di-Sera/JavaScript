920785f7c5060618d57afd0b1035b5169a8305d1
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-01-12 14:42:14 +0100

    exo7.1

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,16 @@
 console.log("exo-7");
 
+
+let traduction = {
+    car: "voiture",
+    house: "maison",
+    game: "jeux",
+    videogame: "jeux video",
+    show: "spectacle",
+};
+jsonDatas.forEach((e) => {
+    e.trad = traduction[e.type];
+});
 console.log(jsonDatas);
+console.log(jsonDatas);
+
